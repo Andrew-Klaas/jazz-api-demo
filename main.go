4e001cb1d5f527e0594f324d72eadc025ebe97d3
Andrew Klaas <aklaas@hashicorp.com>
2022-02-08 10:33:34 -0600

    update

--- main.go
+++ main.go
@@ -17,6 +17,8 @@ var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "john Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "4", Title: "Fellowship of the Ring", Artist: "Tolkien", Price: 39.99},
+
 }
 
 func getAlbums(c *gin.Context) {
