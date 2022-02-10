7913b832482539db4cb4ffca842c322a0ad77ed4
Andrew Klaas <aklaas@hashicorp.com>
2022-02-10 08:28:04 -0600

    add test scaffold

---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(t *testing.T) {
+	fmt.Printf("Hello World!")
+}
+
+func TestGetAlbumsByID(t *testing.T) {
+	fmt.Printf("Hello World!")
+}
